6aa2c19630b51ee991ff039e14d788909cf73a9f
Alejandro García Pérez <alejandro08062@gmail.com>
2026-04-13 12:32:13 +0200

    Added @Bean in PeertubeminerApplication.java

--- PeerTube Miner/src/main/java/aiss/peertubeminer/PeertubeminerApplication.java
+++ PeerTube Miner/src/main/java/aiss/peertubeminer/PeertubeminerApplication.java
@@ -2,6 +2,9 @@ package aiss.peertubeminer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PeertubeminerApplication { // http://localhost:8081
@@ -10,4 +13,8 @@ public class PeertubeminerApplication { // http://localhost:8081
 		SpringApplication.run(PeertubeminerApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
