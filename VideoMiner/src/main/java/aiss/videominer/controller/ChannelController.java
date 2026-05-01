c6aa83705fa264e8926c446ff4f835e5001cbc92
AlonsoTorresG <sebtorgar@alum.us.es>
2026-05-01 17:01:16 +0200

    Channel Controller finished

--- VideoMiner/src/main/java/aiss/videominer/controller/ChannelController.java
+++ VideoMiner/src/main/java/aiss/videominer/controller/ChannelController.java
@@ -1,26 +1,81 @@
 package aiss.videominer.controller;
 
 
+import aiss.videominer.exception.VideoNotFoundException;
+import aiss.videominer.model.Channel;
+import aiss.videominer.model.Video;
+import aiss.videominer.repository.ChannelRepository;
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController()
-@RequestMapping("/videominer/channels")
+@RequestMapping("/videominer/api/v1")
 public class ChannelController {
-    private final ChannelRepository channelRepository;
 
     @Autowired
-    public ChannelController(ChannelRepository channelRepository) {this.channelRepository = channelRepository}
+    ChannelRepository channelRepository;
 
-    // Get list of channels
+    //Endpoint to find all the channels inside the database
+    //GET http://localhost:8080/apipath/channels
     @GetMapping
     public List<Channel> findAll() {return channelRepository.findAll();}
 
-    // Add channels
+
+    //Endpoint to find the channel by the id if it is in the database
+    //GET http://localhost:8080/apipath/channels/{channelId}
+    @GetMapping("/{channelId}")
+    public Channel findOne(@PathVariable String id)
+    {
+        Optional<Channel> channel = channelRepository.findById(id);
+        if (channel.isEmpty()) {
+            return null;
+        }
+        return channel.get();
+    }
+
+
+    // Endpoint to create a channel
+    // POST http://localhost:8080/videominer/api/v1/channels
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public Channel create(@Valid @RequestBody Channel channel) { return channelRepository.create(channel);}
+    public Channel create(@Valid @RequestBody Channel channel) {
+        Channel channel1 = channelRepository.save(
+                new Channel(channel.getName(), channel.getDescription(), channel.getCreatedTime())
+        );
+        return channel1;
+    }
 
-    // Post to get a channel, and add new channel data
+    // Endpoint to update a channel
+    // PUT http://localhost:8080/videominer/api/v1/channels/{channelId}
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping({"/channelId"})
+    public void update(@Valid @RequestBody Channel updatedChannel, @PathVariable String id) throws Exception {
+        Optional<Channel> channelData = channelRepository.findById(id);
 
+        if (channelData.isPresent()) {
+            throw new Exception();
+        }
+        Channel channel1 = channelData.get();
+        channel1.setName(updatedChannel.getName());
+        channel1.setDescription(updatedChannel.getDescription());
+        channel1.setCreatedTime(updatedChannel.getCreatedTime());
+        channelRepository.save(channel1);
+    }
 
+    // Endpoint to delete a channel by its id
+    // DELETE http://localhost:8080/videominer/api/v1/channels/{channelId}
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping({"/channels/{channelId}"})
+    public void delete(@PathVariable String id) {
+        if (channelRepository.existsById(id)) {
+            channelRepository.deleteById(id);
+        }
+    }
 
 }
