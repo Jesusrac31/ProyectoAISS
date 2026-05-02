1fd5351dcb8bc2c53c24ecfc39efeaaacc0be7c0
Jesusrac31 <jesusraceros@gmail.com>
2026-05-02 16:18:43 +0200

    Update start.sh for making easier the start of the deployment

--- start.sh
+++ start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-echo "Starting DailyMotion Miner in port 8081..."
+echo "Iniciando DailyMotion Miner en puerto 8081..."
 java -Xmx120m -jar /app/dailymotion.jar --server.address=127.0.0.1 --server.port=8081 &
+# Le damos 15 segundos enteros de CPU exclusiva a esta app
+sleep 15 
 
-echo "Starting PeerTube Miner in port 8082..."
+echo "Iniciando PeerTube Miner en puerto 8082..."
 java -Xmx120m -jar /app/peertube.jar --server.address=127.0.0.1 --server.port=8082 &
+sleep 15
 
-echo "Starting VideoMiner in port 8083..."
+echo "Iniciando VideoMiner en puerto 8083..."
 java -Xmx120m -jar /app/videominer.jar --server.address=127.0.0.1 --server.port=8083 &
-
-# Esperar a que las apps respiren un poco
-sleep 12
+sleep 15
 
 echo "Iniciando Caddy Gateway..."
 caddy run --config /app/Caddyfile --adapter caddyfile
